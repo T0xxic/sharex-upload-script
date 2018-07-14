2d5e0d2f9ab63b62170882b66b72f58134d48f68
Phillip L <p410n3@gmail.com>
2018-07-14 13:40:13 +0200

    Uploads images to current dir instread of /i/

--- public/index.php
+++ public/index.php
@@ -2,13 +2,13 @@
 header("Content-Type: text/text");
  //the following code will delete images in the /i/ folder after 30 days, you can delete/comment it out if you want.
 $days = 30;
-$dir = "i/";
+$dir = "";
  
 $nofiles = 0;
  
-    if ($handle = opendir($dir)) {
+    if ($handle = opendir('./')) {
     while (( $file = readdir($handle)) !== false ) {
-        if ( $file == '.' || $file == '..' || $file == '.htaccess' || is_dir($dir.'/'.$file) ) {
+        if ( $file == '.' || $file == '..' || $file == '.htaccess'|| $file == 'index.php' || is_dir($dir.'/'.$file) ) {
             continue;
         }
  
@@ -20,17 +20,17 @@ $nofiles = 0;
     closedir($handle);
 }
  
-$key = "YOUR_PASSWORD_HERE";
-$uploadhost = "http://yourwebsite.com/";
-$redirect = "http://yourwebsite.com/";
+$key = "PASSWORD_HERE";
+$uploadhost = "https://i.your.tld";
+$redirect = "http://your.tld";
   
 if (isset($_POST['user'])) {
     if ($_POST['user'] == $key) {
         $target = getcwd() . "/" . basename($_FILES['form']['name']);
         if (move_uploaded_file($_FILES['form']['tmp_name'], $target)) {
             $md5 = md5_file(getcwd() . "/" . basename($_FILES['form']['name']));
-            rename(getcwd() . "/" . basename($_FILES['form']['name']), getcwd() . "/i/" . $md5 . "." . end(explode(".", $_FILES["form"]["name"])));
-            echo $uploadhost . "i/" . $md5 . "." . end(explode(".", $_FILES["form"]["name"]));
+            rename(getcwd() . "/" . basename($_FILES['form']['name']), getcwd() . "/" . $md5 . "." . end(explode(".", $_FILES["form"]["name"])));
+            echo $uploadhost . "/" . $md5 . "." . end(explode(".", $_FILES["form"]["name"]));
         } else {
             echo "Sorry, there was a problem uploading your file.";
         }
